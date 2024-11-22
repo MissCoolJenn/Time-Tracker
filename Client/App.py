d5894cc2cb46dde3f836864bd3e1fdd97e0a36f4
Jenn <feya.magii@gmail.com>
2024-11-22 18:34:11 +0200

    + команда для сборки приложения в .ехе

--- Client/App.py
+++ Client/App.py
@@ -8,3 +8,5 @@ eel.start(
     'Main.html',
     size=(800, 600),  # Указываем размер окна
 )
+
+# pyinstaller --noconfirm --onefile --noconsole --add-data "web;web" --add-data "d:/Dev/axaxa/.venv/lib/site-packages/eel/eel.js;eel" App.py
