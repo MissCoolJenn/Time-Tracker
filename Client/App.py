c3d8350df7a01bd3c02463842f959a135ebd204e
Jenn <feya.magii@gmail.com>
2024-11-22 12:58:37 +0200

    xaxaxax

--- Client/App.py
+++ Client/App.py
@@ -1,7 +1,10 @@
 import eel
 
 # Инициализация папки web
-eel.init('Client\web')
+eel.init('Client/web')
 
-# Запуск HTML-файла
-eel.start('Main.html', size=(300, 300), port=8008)
+# Запускаем Eel
+eel.start(
+    'Main.html',
+    size=(800, 600),  # Указываем размер окна
+)
