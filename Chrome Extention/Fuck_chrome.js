14ba813233540241bb89d4a1ee68b884989b4983
Jenn <feya.magii@gmail.com>
2024-12-01 03:53:22 +0200

    готовый функционал: работа в фоне, получение url активной вкладки, сохранение данных о количестве проведенного времени на вкладке в chrome.storage.local

--- Chrome Extention/Fuck_chrome.js
+++ Chrome Extention/Fuck_chrome.js
@@ -1,10 +1,19 @@
+// Проверка активно ли окно браузера сейчас
+async function is_active_browser() {
+    const window = await new Promise((resolve) => {
+        chrome.windows.getCurrent({ populate: false }, resolve)
+    });
+    return window.focused;
+}
+
 // Получение информации о пользователе
-function get_user_info() {
+async function get_user_info() {
     return new Promise((resolve) => {
         chrome.identity.getProfileUserInfo((userInfo) => {
             if (userInfo) {
-                var date_now = Date.now();
-                //console.log(`\nDate: ${date_now}`);
+                let date_now = Date.now();
+
+                //console.log(\nDate: ${date_now});
                 //console.log("Email:", userInfo.email); // Email пользователя
                 //console.log("ID:", userInfo.id); // ID пользователя
                 
@@ -19,68 +28,129 @@ function get_user_info() {
     });
 }
 
-
 // Проверка какая сейчас вкладка открыта в гугле
-function log_active_tab_url() {
+async function log_active_tab_url() {
     return new Promise((resolve) => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
-            const activeTab = tabs[0];
+            let activeTab = tabs[0];
 
             if (activeTab) {
-                re_url(activeTab.url)//.then(console.log)
-                resolve(activeTab.url); // Возвращаем URL активной вкладки
+                let clear_url = re_url(activeTab.url);  //.then(console.log)
+                resolve(clear_url); // Возвращаем URL активной вкладки
             } 
             else {
                 resolve('NaN'); // Возвращаем NaN, если вкладок нет
             }
-        });
-    });
+        })
+    })
 }
 
-
 // Нахождение url и очистка его от лишних символов
-function re_url(url) {
-    return new Promise((resolve, reject) => {
+async function re_url(url) {
+    return new Promise((resolve) => {
         // Если url начинается с 'https://' 
         const match = url.match(/^https?:\/\/[^\/]+\/?/);
 
-        // https://www.youtube.com/watch?v=pfiCN...
-        // Https://claude.ai/chat/a3f74ee5...
-        // chrome://newtab/
-        // chrome://history/
-
         if (match !== null) {
-            let match_s = match[0].replace(/^.+\/\//, '');   // Убирается начало htpps://
-            let match_e = match_s.replace(/\/$/, '');        // Убирается все что после .com
-            let match_f = match_e.replace(/^www./, '');      // Убирается www. что иногда присутствуют в начале
-
-            // www.youtube.com - дочистить в некоторых ссылках
+            let match_s = match[0].replace(/^.+\/\//, '');     // Убирается начало htpps://
+            let match_e = match_s.replace(/\/$/, '');          // Убирается все что после .com
+            let match_f = match_e.replace(/^www./, '');        // Убирается www. что иногда присутствуют в начале
 
             console.log(`Активная вкладка сейчас: ${match_f}`)
-            resolve(match_f); // Возвращаем чистый url
+            resolve(match_f);                                  // Возвращаем чистый url
         } 
         else {
-            //resolve("Некорректный URL"); // Или resolve(null) для мягкой обработки
             console.log("Некорректный URL");
         }
     });
 }
 
+// Получение времени на сейчас
+async function get_date_now() {
+    return new Promise((resolve) => {
+        const today = new Date();
+        const timestamp = Date.now();
 
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1;
+        const day = today.getDate();
+        const hours = today.getHours();
+        const minutes = today.getMinutes();
+        const seconds = today.getSeconds();
+        const milliseconds = today.getMilliseconds();
+
+        const date = [timestamp, year, month, day, hours, minutes, seconds, milliseconds];
+        resolve(date);
+    });
+}
+
+// Класс для выполнения задач по обращению в БД и сохранению данных
+class Save_url {
+    async main(url, delta_s, date) {
+        let key = `${date[1]} ${date[2]} ${date[3]}`;
+        console.log(`key: ${key}`);
+
+        // Получение словаря url за сегодня
+        let urls_today = await this.get_storage(key);
+        console.log(`url_today_exist: ${JSON.stringify(urls_today)}`)
+
+        // ПРИМЕР ДАТЫ:
+        // "2024 10 21" = { url: 6404, url: 577 }
+        //                          ^ значение - Сколько сек была активна вкладка
+        //                    ^ ключ - Cслылка
+        //   ^ ключ - День
+
+        // Обновление активного времени url в БД за сегодня
+        if (urls_today.hasOwnProperty(url)) {
+            urls_today[url] = urls_today[url] + delta_s;
+        }
+        // Добавление url в БД за сегодня
+        else {
+            console.log('**Добавление url в БД за сегодня')
+            urls_today[url] = delta_s
+        }
+
+        console.log(`urls_today после просчета времени: ${JSON.stringify(urls_today)}`);
+
+        // Сохранение словаря url за сегодня обратно в БД
+        let save_db = await this.set_storage(key, urls_today);
+        if (save_db) {
+            console.log(`data saved is: ${save_db}`);
+        }
+    };
+
+    async get_storage(key) {
+        return new Promise((resolve) => {
+            chrome.storage.local.get([key], (result) => {
+                resolve(result[key] || {});
+            })
+        })
+    };
+
+    async set_storage(key, value) {
+        return new Promise((resolve) => {
+            chrome.storage.local.set({ [key]: value }, (result) => {
+                resolve(true);
+            })
+        })
+    };
+};
+
+// Класс для работы с БД хрома
 class Data {
     constructor() {
         return;
         // ПРИМЕР ДАТЫ:
-        // {url}: [[2024:10:21, 640], [2024:10:22, 577]]
-        //                      ^ Сколько мин была активна вкладка
-        //              ^ День 
-        //   ^ сслылка
+        // "2024 10 21" = { "url": 6404, "url": 577 }
+        //                          ^ значение - Сколько сек была активна вкладка
+        //                    ^ ключ - Cслылка
+        //   ^ ключ - День
     };
 
-    save(url, time) {
+    async save(url, time) {
         // Если Url ещё раньше не сохранялся в БД
-        if (chrome.storage.local.get(['url']) == undefined) {
+        if (chrome.storage.local.get(['date']) == undefined) {
             
         }
         else {
@@ -88,64 +158,61 @@ class Data {
         }
     };
 
-    rewhrite(url, delta_time) {
+    async rewhrite(url, delta_time) {
         return;
     };
 
-    get_today() {
+    async get_today() {
         return;
     };
 
-    get_all() {
+    async get_all() {
         return;
     };
 
-    delete(date) {
+    async delete(date) {
         return;
     };
-};;
-
+};
 
 // Солнце светит, негры пашут
-// Цикл с асинхронной обработкой
-async function loop() {
-    // Используем промисифицированную обёртку для `chrome.windows.getCurrent`.
-    const window = await new Promise((resolve) => {
-        chrome.windows.getCurrent({ populate: false }, resolve);
-    });
-    
-    // Окно активно 
-    if (window.focused) {
-        // Асинхронный вызов моего говна
-        //const q = await get_user_info(); // Ждём получения информации
-        //if (q[1] == 'NaN') {  // Проверка на NaN
-        //    console.log("Нет активной вкладки, пошел нахуй");
-        //} 
-        //else {
-        //    console.log("Данные пользователя и вкладки:", q);
-        //}
+async function loop(loop_time) {
+    let chrome_active = await is_active_browser();
 
-        await get_user_info();
+    console.log('\n\n')
+
+    if (chrome_active) {
+        // Получение данных о url активной вкладки
+        let user = await get_user_info();
+
+        // Получение времени сейчас 
+        let date_now = await get_date_now();
+
+        // Сохранение в БД данных о url
+        await new Save_url().main(user[1], loop_time/1000, date_now);
     } 
-    // Окно не активно 
     else {
         console.log("Окно неактивно... пока никто не видит z z z z ");
     }
 }
 
+// Удаление полностью всех значений
+chrome.storage.local.clear(function() {
+    console.log('Хранилище очищено.');
+});
+
+let loop_time = 2000;
 // Начало кода и запуск рабского цикла
 console.log('Hi)');
 
-setInterval(loop, 1000);
-
 // Восстановление задач после выхода из сна
 chrome.runtime.onStartup.addListener(() => {
-    console.log("Chrome started, restarting tasks...");
-    setInterval(loop, 1000);
+    console.log("Хром запущен, восстановление потужностi...");
+    setInterval(() => loop(loop_time), loop_time);
 });
-  
+
 // Восстановление при повторном запуске расширения
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed/updated, starting tasks...");
-    setInterval(loop, 1000);
+    console.log("Обновление установлено, запуск потужностi...");
+    setInterval(() => loop(loop_time), loop_time);
 });
