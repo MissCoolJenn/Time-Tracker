c78ce70bc7b2a025c752e4b0087d695f4137c4c6
Jenn <feya.magii@gmail.com>
2024-12-03 16:09:47 +0200

    первая версия рабочего билда с интерфесом и выводом времени

--- Chrome Extention/Hiroshima.js
+++ Chrome Extention/Hiroshima.js
@@ -1,4 +1,4 @@
-function get_date_now() {
+async function get_date_now() {
     return new Promise((resolve) => {
         const today = new Date();
 
@@ -11,7 +11,7 @@ function get_date_now() {
     })
 }
 
-function get_data(key) {
+async function get_data(key) {
     return new Promise((resolve) => {
         chrome.storage.local.get([key], (result) => {
             resolve(result[key] || {});
@@ -19,13 +19,14 @@ function get_data(key) {
     })
 };
 
-function recount_time(data) {
+async function recount_time(data) {
     return new Promise((resolve) => {
         // {"accounts.google.com":4,"epicgames.com":8,"github.com":48,"google.com":16,"nexusmods.com":102,"youtube.com":486}
         let new_data = [];
+        let keys = Object.keys(data)
         
-        for (let i = 0; i > Range(data); i++) {
-            let key = data[i];
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
             let sec = data[key];
             let min = 0;
             let hour = 0;
@@ -46,7 +47,7 @@ function recount_time(data) {
                 }
             }
 
-            time_str = (hour >0 ? hour + 'h' : '') + (min >0 ? min + 'm' : '') + sec + 's';
+            time_str = (hour >0 ? hour + 'h ' : '' ) + (min >0 ? min + 'm ' : '') + sec + 's';
 
             new_data.push(`${key}: ${time_str}`);
         }
@@ -55,27 +56,49 @@ function recount_time(data) {
     })
 }
 
+async function page_elements(data_list) {
+    return new Promise((resolve) => {
+        let main = document.getElementById('main');
 
-function biden() {
+        for (let i = 0; i < data_list.length; i++) {
+            let el_div = document.createElement('div');
+
+            let el_p = document.createElement('p');
+            el_p.textContent = data_list[i];
+            el_div.appendChild(el_p);
+
+            if (i != (data_list.length -1)) {
+                let el_hr = document.createElement('hr');
+                el_div.appendChild(el_hr);
+            }
+
+            main.appendChild(el_div);
+        }
+        resolve();
+    })
+}
+
+
+async function biden() {
     // Получение даты
-    let date = get_date_now();
+    let date = await get_date_now();
 
     console.log(date);
 
     // Запрос данных из БД
-    let data = get_data(date);
+    let data = await get_data(date);
 
     // Пересчет секунд на ч\м\с
-    let data_a = JSON.stringify(data);
-    let data_list = recount_time(data_a);
+    let data_a = data //JSON.stringify(data);
+    let data_list = await recount_time(data_a);
 
     console.log(data_list);
     
     // Вывод данных на страницу
+    await page_elements(data_list);
 }
 
 // Срабатывает при загрузке страницы
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('Hi page)')
     biden();
 });
